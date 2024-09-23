8d1038ae8568520df4bc2ba18c13042ec12eb671
SabrinaBlue <sabrina@bluelogic.com.br>
2024-09-23 08:34:05 +0100

    gatsby-plugin-algolia-search COMMENTED

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,17 +17,19 @@ const pluginsConfig = [
       styles: `${__dirname}/src/styles`,
     },
   },
-  {
-    resolve: `gatsby-plugin-algolia-search`,
-    options: {
-      appId: process.env.GATSBY_ALGOLIA_APP_ID,
-      apiKey: process.env.ALGOLIA_ADMIN_KEY,
-      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-      queries,
-      chunkSize: 10000, // default: 1000
-      enablePartialUpdates: true,
-    },
-  },
+  /*{
+     resolve: `gatsby-plugin-algolia-search`,
+     options: {
+       appId: process.env.GATSBY_ALGOLIA_APP_ID,
+       apiKey: process.env.ALGOLIA_ADMIN_KEY,
+       indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+       queries,
+       chunkSize: 10000, // default: 1000
+       enablePartialUpdates: true,
+     },
+   },
+   */
+
   // needs to be the first to work with gatsby-remark-images
   {
     resolve: `gatsby-source-filesystem`,
